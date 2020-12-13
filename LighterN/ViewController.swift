58c07758ef8c99632530883b1371739b99407f2d
Nikita Kuzmin <Dr.Cappo@gmail.com>
2020-12-13 03:37:49 +0300

    Added Icons And Screens For ReadMe. Functional is fine. Need to fix button Layout.

--- LighterN/ViewController.swift
+++ LighterN/ViewController.swift
@@ -10,42 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var isLightOn = true
+    var backgroundColor = 0
     var isRealLightOn = false
+    let button = UIButton(type: .system)
     
     override var prefersStatusBarHidden: Bool{
         return true
     }
     
-    @objc func buttonAction(button: UIButton){
+    @objc func buttonAction(){
+        isRealLightOn.toggle()
         if isRealLightOn{
-            isRealLightOn.toggle()
-            button.setTitle("Turn On", for: .normal)
-            updateView()
+            button.setTitle("Torch Off", for: .normal)
         } else {
-            isRealLightOn.toggle()
-            button.setTitle("Turn Off", for: .normal)
-            updateView()
+            button.setTitle("Torch On", for: .normal)
         }
+        updateView()
     }
     
     func createButton(){
-        let button = UIButton(type: .system)
         button.frame = CGRect(x: self.view.frame.midX - self.view.frame.midX/2, y: self.view.frame.maxY - self.view.frame.midY/2, width: 200, height: 50)
-        button.backgroundColor = .cyan
-        button.setTitle("Turn On", for: .normal)
+        button.backgroundColor = .black
+        button.setTitle("Torch On", for: .normal)
         button.layer.cornerRadius = 25
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        button.tintColor = .white
         
         self.view.addSubview(button)
     }
     
+    /// Control the Device Torch Status
     func updateView() {
             let device = AVCaptureDevice.default(for: AVMediaType.video)
             
             if let dev = device, dev.hasTorch {
-                view.backgroundColor = .black
                 do {
                     try dev.lockForConfiguration()
                     dev.torchMode = isRealLightOn ? .on : .off
@@ -54,7 +53,7 @@ class ViewController: UIViewController {
                     print(error)
                 }
             } else {
-                print("No FlashLight")
+                print("Device Has No Torch.")
             }
         }
     
@@ -65,12 +64,22 @@ class ViewController: UIViewController {
     }
 
     fileprivate func updateUI() {
-        view.backgroundColor = isLightOn ? .white: .black
+        
+        switch backgroundColor {
+        case 1:
+            view.backgroundColor = .yellow
+        case 2:
+            view.backgroundColor = .green
+        default:
+            view.backgroundColor = .red
+        }
+        backgroundColor += 1
+        if backgroundColor == 3{
+            backgroundColor = 0
+        }
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        isLightOn.toggle()
         updateUI()
     }
 }
-
-
